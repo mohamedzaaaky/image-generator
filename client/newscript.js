42a79d4c082c0c389d44937dde410cd365f7f890
Mohamed Zaki <mohamedzakiii667@gmail.com>
2025-03-22 06:23:37 +0200

    تعديل server.js و fetch link في الفرونت

--- client/newscript.js
+++ client/newscript.js
@@ -127,7 +127,7 @@ const generateImages = async (
 
   const imagePromises = Array.from({ length: imageCount }, async (_, i) => {
     try {
-      const response = await fetch("https://discreet-cobalt-amazonsaurus.glitch.me/generate", {
+      const response = await fetch("/generate", {
 
         method: "POST",
         headers: {
